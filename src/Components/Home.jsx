a5fca6a0c40a32ce1af93419b09116f186b07ce0
Hannan <l1f21bsse0375@ucp.edu.pk>
2025-02-26 21:24:46 +0500

    final commit after some changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ function Home({ Todo, Delete, Update, onDragStart, onDrop, onDragOver }) {
     return (
         <>
             <div className='flex justify-evenly mt-3 text-2xl font-bold '>
-                <div className='h-screen bg-green-600   w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
+                <div className='h-auto bg-green-600 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
                     onDrop(e, "ToDo")
                 }}>
                     <h2 className="mb-4 text-white ">To Do</h2>
@@ -18,7 +18,7 @@ function Home({ Todo, Delete, Update, onDragStart, onDrop, onDragOver }) {
                     </div>
                 </div>
 
-                <div className='h-screen bg-orange-500 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
+                <div className='h-auto bg-orange-500 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
                     onDrop(e, "In Progress")
                 }}>
                     <h2 className="mb-4 text-white">In Progress</h2>
@@ -29,7 +29,7 @@ function Home({ Todo, Delete, Update, onDragStart, onDrop, onDragOver }) {
                     </div>
                 </div>
 
-                <div className='h-screen bg-red-600 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
+                <div className='h-auto bg-red-600 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
                     onDrop(e, "Done")
                 }}>
                     <h2 className="mb-4 text-white">Done</h2>
