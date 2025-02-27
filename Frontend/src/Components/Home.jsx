eebc1c38c9fd01656e8e38842cc5134ef97c09a8
Hannan <l1f21bsse0375@ucp.edu.pk>
2025-02-27 15:22:47 +0500

    final commit with priority

---
+++ Frontend/src/Components/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+import Card from './Card';
+
+function Home({ Todo, Delete, Update, onDragStart, onDrop, onDragOver }) {
+    return (
+        <>
+            <div className='flex justify-evenly mt-3 text-2xl font-bold h-[100vh] '>
+                <div className=' bg-green-600 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
+                    onDrop(e, "ToDo")
+                }}>
+                    <h2 className="mb-4 text-white ">To Do</h2>
+                    <div className="space-y-4 ">
+                        {Todo.filter((task) => task && task.status === "ToDo").map((task, index) => (
+                            <Card key={task.title} task={task} Delete={Delete} Update={Update} onDragStart={onDragStart} index={index} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className='h-auto bg-orange-500 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
+                    onDrop(e, "In Progress")
+                }}>
+                    <h2 className="mb-4 text-white">In Progress</h2>
+                    <div className="space-y-4 ">
+                        {Todo.filter((task) => task && task.status === "In Progress").map((task, index) => (
+                            <Card key={task.title} task={task} Delete={Delete} Update={Update} onDragStart={onDragStart} index={index} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className='h-auto bg-red-600 w-1/3 text-center pt-4' onDragOver={onDragOver} onDrop={(e) => {
+                    onDrop(e, "Done")
+                }}>
+                    <h2 className="mb-4 text-white">Done</h2>
+                    <div className="space-y-4">
+                        {Todo.filter((task) => task && task.status === "Done").map((task, index) => (
+                            <Card key={task.title} task={task} Delete={Delete} Update={Update} onDragStart={onDragStart} index={index} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <section>
+                <div className='bg-black h-12 text-white text-4xl text-center'>KanBan</div>
+            </section>
+        </>
+    )
+}
+
+export default Navbar
